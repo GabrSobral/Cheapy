c6ed37030322918b49af3be80a245979a9987a37
Sobraloser <Asgoth55@gmail.com>
2021-12-28 22:44:17 -0300

    feat: add banner

---
+++ src/components/Banner/index.tsx
@@ -0,0 +1,9 @@
+import styles from './style.module.scss'
+
+export const Banner = () => {
+  return(
+    <section className={styles.container}>
+
+    </section>
+  )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,21 @@
 import type { NextPage } from 'next'
+import { Banner } from '../components/Banner'
 import { Header } from '../components/Header'
+
+import styles from '../styles/home.module.scss'
 
 const Home: NextPage = () => {
   return (
-   <div>
+   <div className={styles.container}>
      <Header/>
+     <Banner/>
+
+      <main>
+        <section>
+          <span className={styles.titleSection}>Anunciados hoje</span>
+
+        </section>
+      </main>
    </div>
   )
 }
