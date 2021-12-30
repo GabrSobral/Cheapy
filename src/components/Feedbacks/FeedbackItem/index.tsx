0073cb334818a8b3e4762af6cc881d77892ca72e
Sobraloser <Asgoth55@gmail.com>
2021-12-30 17:22:02 -0300

    feat: add FeedbackItem

--- src/components/Feedbacks/FeedbackItem/index.tsx
+++ src/components/Feedbacks/FeedbackItem/index.tsx
@@ -1,17 +1,34 @@
 import Image from 'next/image'
+import { IFeedback } from '../../../types/IFeedback'
 import { FeedbackStars } from '../../FeedbackStars'
 import styles from './style.module.scss'
 
-export const FeedbackItem = () => {
+interface Props {
+  feedback: IFeedback;
+}
+
+export const FeedbackItem = ({ feedback }: Props) => {
   return(
     <div className={styles.container}>
       <div className={styles.top}>
         <div>
-          <Image src="/img1" alt="foto de perfil" width={48} height={48}/>
-          <span>Ana Silva Dos Santos</span>
+          <div className={styles.img}>
+            <Image src="/img1.jpg" alt="foto de perfil" width={48} height={48}/>
+          </div>
+          <span>{feedback.user.name}</span>
         </div>
 
-        <FeedbackStars size={25}/>
+        <FeedbackStars size={32} stars={feedback.stars}/>
+      </div>
+
+      <div className={styles.content}>
+        <span className={styles.stitle}>{feedback.title}</span>
+        <p>{feedback.content}</p>
+      </div>
+
+      <div className={styles.recomendation} style={{ color: feedback.recomendation ? "#8FE281" : "#E45353" }}>
+        <Image src={`/${feedback.recomendation ? "check" : "x"}.svg`} alt="check" width={24} height={24}/>
+        {feedback.recomendation ? "Recomendo este produto" : "Não recomendo este produto"}
       </div>
     </div>
   )
