f7f01b374a77f10e28898104736972434b5a7d00
Sobraloser <Asgoth55@gmail.com>
2021-12-30 17:21:18 -0300

    feat: turn feedbackStars dynamic

--- src/components/FeedbackStars/index.tsx
+++ src/components/FeedbackStars/index.tsx
@@ -1,17 +1,18 @@
 import Image from 'next/image'
 
 interface Props {
-  size: number
+  size: number;
+  stars: number;
 }
 
-export const FeedbackStars = ({ size }: Props) => {
+export const FeedbackStars = ({ size,stars }: Props) => {
   return(
     <div>
-      <Image src="/StarFilled.svg" alt="Star of feedback" width={size} height={size}/>
-      <Image src="/StarFilled.svg" alt="Star of feedback" width={size} height={size}/>
-      <Image src="/StarFilled.svg" alt="Star of feedback" width={size} height={size}/>
-      <Image src="/Star.svg" alt="Star of feedback" width={size} height={size}/>
-      <Image src="/Star.svg" alt="Star of feedback" width={size} height={size}/>
+      <Image src={`/Star${stars >= 1 ? "Filled" : ""}.svg`} alt="Star of feedback" width={size} height={size}/>
+      <Image src={`/Star${stars >= 2 ? "Filled" : ""}.svg`} alt="Star of feedback" width={size} height={size}/>
+      <Image src={`/Star${stars >= 3 ? "Filled" : ""}.svg`} alt="Star of feedback" width={size} height={size}/>
+      <Image src={`/Star${stars >= 4 ? "Filled" : ""}.svg`} alt="Star of feedback" width={size} height={size}/>
+      <Image src={`/Star${stars >= 5 ? "Filled" : ""}.svg`} alt="Star of feedback" width={size} height={size}/>
     </div>
   )
 }
