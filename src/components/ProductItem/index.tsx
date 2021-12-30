c68a214a1df27e6eddd1b1286cae334a827312a0
Sobraloser <Asgoth55@gmail.com>
2021-12-30 17:25:12 -0300

    feat: turn Product Item dynamic

--- src/components/ProductItem/index.tsx
+++ src/components/ProductItem/index.tsx
@@ -2,28 +2,41 @@ import styles from './style.module.scss'
 import Image from 'next/image'
 import { FeedbackStars } from '../FeedbackStars'
 import Link from 'next/link'
+import { IProduct } from '../../types/IProduct'
+import { useEffect, useState } from 'react'
 
-export const ProductItem = () => {
+interface Props {
+  product: IProduct;
+}
+
+export const ProductItem = ({ product }: Props) => {
+  const [ currentPrice, setCurrentPrice ] = useState(0);
+
+  useEffect(() => {
+    if(!product?.price) return;
+    
+    const discount = (product.price * product.discount)/100
+    setCurrentPrice(product.price - discount)
+  },[product])
+
   return(
     <Link href="/product" passHref>
       <div className={styles.container}>
         <div className={styles.imageContainer}>
           <div className={styles.discount}>
-            <span>-10%</span>
+            <span>-{product.discount}%</span>
           </div>
           <Image src="/img1.jpg" alt="imagem do produto" layout="fill" objectFit="cover"/>
         </div>
 
         <div className={styles.detailsContainer}>
-          <span className={styles.productName}>
-            Smartphone Samsung Galaxy A32 128GB Violeta 4G
-          </span>
+          <span className={styles.productName}>{product.name}</span>
 
-          <FeedbackStars size={25}/>
+          <FeedbackStars size={25} stars={product.average_rating}/>
 
           <div className={styles.priceContainer}>
-            <span className={styles.oldPrice}>R$1.999.00</span>
-            <span className={styles.currentPrice}>R$1.349.00</span>
+            <span className={styles.oldPrice}>{`R$${product.price}`}</span>
+            <span className={styles.currentPrice}>{`R$${currentPrice}`}</span>
           </div>
         </div>
       </div>
