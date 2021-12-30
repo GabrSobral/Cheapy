48bf64711abe28876aade349ddab77419d23578c
Sobraloser <Asgoth55@gmail.com>
2021-12-30 17:22:37 -0300

    feat: add feedbacks dynamic

--- src/components/Feedbacks/index.tsx
+++ src/components/Feedbacks/index.tsx
@@ -1,7 +1,10 @@
 import { FeedbackItem } from './FeedbackItem'
 import styles from './style.module.scss'
 
+import items from '../../mocks/feedbacks.json'
+
 export const Feedbacks = () => {
+
   return(
     <section className={styles.feedbacks_container}>
       <div className={styles.feedbacks_container_top}>
@@ -13,7 +16,7 @@ export const Feedbacks = () => {
       </div>
 
       <div className={styles.feedbacks_list}>
-        <FeedbackItem/>
+        {items.map(item => <FeedbackItem key={item.id} feedback={item}/>)}
       </div>
     </section>
   )
