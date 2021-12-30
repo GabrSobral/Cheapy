6f00457941bd21f42e70e353e8a211a6a7466b6e
Sobraloser <Asgoth55@gmail.com>
2021-12-30 11:51:11 -0300

    feat: add link to logo

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,14 @@
 import Image from 'next/image'
+import Link from 'next/link'
 
 import styles from './style.module.scss'
 
 export const Header = () => {
   return(
     <header className={styles.container}>
-      <Image src="/Logo.svg" alt="Logo" height={50} width={120}/>
+      <Link href="/" passHref>
+        <Image src="/Logo.svg" alt="Logo" height={50} width={120}/>
+      </Link>
 
       <nav>
         <a href="#">Contato</a>
