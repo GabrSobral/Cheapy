2ad3d0c27895cdac290319411cdf6be32d52be0d
Sobraloser <Asgoth55@gmail.com>
2022-01-10 09:08:37 -0300

    feat: add auth state do header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,12 @@
 import Image from 'next/image'
 import Link from 'next/link'
+import { useState } from 'react';
 
 import styles from './style.module.scss'
 
 export const Header = () => {
+  const [ isAuthenticated ] = useState(true);
+
   return(
     <header className={styles.container}>
       <Link href="/">
@@ -12,8 +15,31 @@ export const Header = () => {
 
       <nav>
         <a href="#">Contato</a>
-        <Link href="/SignIn"><a>Entrar</a></Link>
+        { !isAuthenticated ?
+          <Link href="/SignIn"><a>Entrar</a></Link> :
+          <Link href="/"><a>Meu carrinho</a></Link>
+        }
         <a href="#" className={styles.announce}>Anunciar</a>
+        
+        { isAuthenticated && (
+          <div className={styles.user_container}>
+            <Link href="/Profile">
+              <a className={styles.image_container}>
+                <Image 
+                  src="https://github.com/GabrSobral.png" 
+                  alt="Imagem do usuário"
+                  width={48}
+                  height={48}
+                  objectFit="cover"
+                  placeholder="blur"
+                  blurDataURL="https://github.com/GabrSobral.png"
+
+                />
+              </a>
+            </Link>
+            <Link href="/Profile"><a>Olá, Gabriel</a></Link>
+          </div>
+        ) }
       </nav>
     </header>
   )
