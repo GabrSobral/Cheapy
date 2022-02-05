897499abd56fc3100bbce4685d5cd9f9f5012f13
Sobraloser <Asgoth55@gmail.com>
2022-02-04 21:48:25 -0300

    feat: add refresh token logic

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,15 +1,27 @@
 import axios from 'axios'
-import { getToken } from '../utils/JsonWebToken';
+import { getToken, removeToken, setToken } from '../utils/JsonWebToken';
+import { GetUserId, IsTokenExpired } from '../utils/parseJWT';
+import { getRefreshToken } from '../utils/RefreshToken';
 
 export const api = axios.create({
   baseURL: "https://localhost:5001/v1"
 });
 
 const token = getToken();
+const refreshToken = getRefreshToken();
 
-if(token) {
-  api.interceptors.request.use(config => {
-    config.headers && (config.headers.authorization = `Bearer ${token}`);
-    return config;
-  });
-}
+token && api.interceptors.request.use(async config => {
+  config.headers && (config.headers.authorization = `Bearer ${token}`);
+
+  if(refreshToken && IsTokenExpired()) {
+    const { data } = 
+      await axios.post(`https://localhost:5001/v1/refresh-token/${refreshToken}`,
+      { userId: GetUserId() },
+      { headers: { Authorization : `Bearer ${token}` }}
+    );
+    removeToken();
+    setToken(data.token);
+  }
+
+  return Promise.resolve(config);
+}, (error) => api(error.config));
