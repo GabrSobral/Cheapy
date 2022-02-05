a55af1c6fc32363493f8cafb69eff9934cff9030
Sobraloser <Asgoth55@gmail.com>
2022-02-04 21:48:10 -0300

    fix: tokens

--- src/utils/RefreshToken.ts
+++ src/utils/RefreshToken.ts
@@ -1,14 +1,14 @@
 import { setCookie, parseCookies, destroyCookie } from 'nookies'
 
-const token_name = "@Cheapy:jwtToken";
+const token_name = "@Cheapy:refreshToken";
 
 export const setRefreshToken = (token: string) => {
   setCookie(undefined, token_name, token, {
     maxAge: 60 * 60 * 24 * 4 //4 days
   });
 }
-export const getToken = () => {
-  const { "@Cheapy:jwtToken": refreshToken } = parseCookies();
+export const getRefreshToken = () => {
+  const { "@Cheapy:refreshToken": refreshToken } = parseCookies();
   return refreshToken;
 }
 export const removeRefreshToken = () => {
