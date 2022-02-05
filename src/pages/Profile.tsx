bd82baf8f8d7615aaaa5615eb58ab3fe6b79c7e1
Sobraloser <Asgoth55@gmail.com>
2022-02-04 21:46:41 -0300

    refact

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,7 +1,17 @@
 import { NextPage } from "next"
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 import { ProfileContent } from "../content/ProfileContent";
+import { getToken } from "../utils/JsonWebToken";
 
 const Profile: NextPage = () => {
+  const { replace } = useRouter();
+
+  useEffect(() => {
+    if(!getToken())
+      replace("/");
+  },[replace])
+  
   return <ProfileContent/>
 }
 export default Profile;
